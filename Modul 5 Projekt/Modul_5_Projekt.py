c41b224a9bc044c940f52a7b40df4f3a57063963
KolosovB <boris.kolosov@mail.de>
2024-06-22 15:27:58 +0200

    22.06.2024 Boris

--- Modul 5 Projekt/Modul_5_Projekt.py
+++ Modul 5 Projekt/Modul_5_Projekt.py
@@ -14,80 +14,12 @@ from PIL import Image, ImageTk as pil
 import os
 import csv
 from encodings import utf_8
+import Gui_Windows as win
 
-class maingui:
-    def __init__(self, root, title, geometry):
-
-        self.root = root
-        self.root.title(title)
-        self.root.geometry(geometry)
-        self.pageshow = Login_Page(self, self.root)
-      
-    def changepage(self, page):
-        self.page = page
-        
-        if self.page == 0:
-            #del self.pageshow
-            self.pageshow = Login_Page(self, self.root)
-
-        if self.page == 1:
-            #del self.pageshow
-            self.pageshow = Sign_Page(self, self.root)
-
-
-class Login_Page:
-    def __init__(self, parent, window):
-        
-        self.parent = parent
-        
-        self.frame = ttk.Frame(window)
-        self.frame.pack()
-
-        self.welcm_lbl = ttk.Label(self.frame, text='welcome')
-        self.welcm_lbl.grid(row=0, column=1)
-
-        self.name_lbl = ttk.Label(self.frame, text='name:')
-        self.name_lbl.grid(row=1, column=0)
-
-        self.name_entry = ttk.Entry(self.frame)
-        self.name_entry.grid(row=1, column=1)
-
-        self.sbt = ttk.Button(self.frame, text='login', command=self.clicked)
-        self.sbt.grid(row=2, column=1)
-
-    def clicked(self):
-        self.frame.destroy()
-        self.parent.changepage(1)
-        
-
-class Sign_Page():
-    def __init__(self, parent, window):
-
-        self.parent = parent
-        
-        self.frame = tk.Frame(window)
-        self.frame.pack()
-
-        self.welcm_lbl = tk.Label(self.frame, text='welcome sign-up')
-        self.welcm_lbl.grid(row=0, column=1)
-
-        self.name_lbl = tk.Label(self.frame, text='name:')
-        self.name_lbl.grid(row=1, column=0)
-
-        self.name_entry = tk.Entry(self.frame)
-        self.name_entry.grid(row=1, column=1)
-
-        self.sbt = tk.Button(self.frame, text='sign-up', command=self.clicked)
-        self.sbt.grid(row=2, column=1)      
-
-    def clicked(self):
-        self.frame.destroy()
-        self.parent.changepage(0)
-        
     
 def main():
     root = tk.Tk()
-    maingui(root, "Rpg", "400x400")
+    win.maingui(root, "Mitarbeiterverwaltung")
     root.mainloop()
     
 if __name__ =='__main__':
