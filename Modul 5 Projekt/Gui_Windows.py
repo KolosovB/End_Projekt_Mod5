c61833a1ece61b612e73fe5dc01a2c37fed60623
KolosovB <boris.kolosov@mail.de>
2024-06-23 18:06:16 +0200

    1

--- Modul 5 Projekt/Gui_Windows.py
+++ Modul 5 Projekt/Gui_Windows.py
@@ -38,9 +38,8 @@ class Start_Page:
         global points
         points = []
         
-        fcon.check_conf_file()
         date_lst = []
-        fcon.read_conf_file(date_lst)
+        self.sql_check(date_lst)
         
         self.parent = parent
         
@@ -56,43 +55,55 @@ class Start_Page:
         self.label_img = ttk.Label(self.frame, image=self.tkimage)
         self.label_img.place(x = ((points[2]-int(width/8))/2), y = ((points[1]-int(height/8))/2))
         
-        self.welcm_lbl = ttk.Label(self.frame, text='DB Connect')
+        self.welcm_lbl = ttk.Label(self.frame, text='DB Connection')
         self.welcm_lbl.place(x = points[4], y = (((points[1]-int(height/8))/2) + 0))
 
-        self.name_lbl1 = ttk.Label(self.frame, text='Server:')
-        self.name_lbl1.place(x = points[4], y = (((points[1]-int(height/8))/2) + 26))
+        self.serv_lbl = ttk.Label(self.frame, text='Server:')
+        self.serv_lbl.place(x = points[4], y = (((points[1]-int(height/8))/2) + 26))
 
-        self.name_entry1 = WPEntry(self.frame, date_lst[0][1])
-        self.name_entry1.place(x = points[4], y = (((points[1]-int(height/8))/2) + 46))
+        self.serv_entr = WPEntry(self.frame, date_lst[0][1])
+        self.serv_entr.place(x = points[4], y = (((points[1]-int(height/8))/2) + 46))
         
-        self.name_lbl2 = ttk.Label(self.frame, text='Database:')
-        self.name_lbl2.place(x = points[4], y = (((points[1]-int(height/8))/2) + 76))
+        self.db_lbl = ttk.Label(self.frame, text='Database:')
+        self.db_lbl.place(x = points[4], y = (((points[1]-int(height/8))/2) + 76))
 
-        self.name_entry2 = WPEntry(self.frame, date_lst[1][1])
-        self.name_entry2.place(x = points[4], y = (((points[1]-int(height/8))/2) + 96))
+        self.db_entr = WPEntry(self.frame, date_lst[1][1])
+        self.db_entr.place(x = points[4], y = (((points[1]-int(height/8))/2) + 96))
         
-        self.name_lbl3 = ttk.Label(self.frame, text='Username:')
-        self.name_lbl3.place(x = points[4], y = (((points[1]-int(height/8))/2) + 126))
+        self.user_lbl = ttk.Label(self.frame, text='Username:')
+        self.user_lbl.place(x = points[4], y = (((points[1]-int(height/8))/2) + 126))
 
-        self.name_entry3 = WPEntry(self.frame, date_lst[2][1])
-        self.name_entry3.place(x = points[4], y = (((points[1]-int(height/8))/2) + 146))
+        self.user_entr = WPEntry(self.frame, date_lst[2][1])
+        self.user_entr.place(x = points[4], y = (((points[1]-int(height/8))/2) + 146))
         
-        self.name_lbl4 = ttk.Label(self.frame, text='Password:')
-        self.name_lbl4.place(x = points[4], y = (((points[1]-int(height/8))/2) + 176))
+        self.pass_lbl = ttk.Label(self.frame, text='Password:')
+        self.pass_lbl.place(x = points[4], y = (((points[1]-int(height/8))/2) + 176))
 
-        self.name_entry4 = WPEntry(self.frame, date_lst[3][1])
-        self.name_entry4.place(x = points[4], y = (((points[1]-int(height/8))/2) + 196))
+        self.pass_entr = WPEntry(self.frame, date_lst[3][1])
+        self.pass_entr.place(x = points[4], y = (((points[1]-int(height/8))/2) + 196))
+        
+        global check_save
+        check_save = ttk.IntVar()
+        self.checkbox = ttk.Checkbutton(self.frame, text="Speichern Einstellungen", variable=check_save)
+        self.checkbox.place(x = points[4], y = (((points[1]-int(height/8))/2) + 236))
 
         self.sbt = ttk.Button(self.frame, text='Login', command=self.clicked)
-        self.sbt.place(x = points[4], y = (((points[1]-int(height/8))/2) + 240))
+        self.sbt.place(x = points[4], y = (((points[1]-int(height/8))/2) + 260))
         
         
     def clicked(self):
         self.frame.destroy()
         self.parent.changepage(1)
         
+    def sql_check(self, lst):
+        
+        fcon.check_conf_file()
+        fcon.read_conf_file(lst)
+
+        return lst
+
     def sql_conf():
-        pass    
+        pass
         
     def take_the_middle(self, par, lis):
         d_width = par.root.winfo_screenwidth()
