6b733affe01dd15fe8630525d0a16911e3d95f6f
KolosovB <boris.kolosov@mail.de>
2024-06-23 16:01:51 +0200

    Placeholder

--- Modul 5 Projekt/Gui_Windows.py
+++ Modul 5 Projekt/Gui_Windows.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox as mb
+from turtle import color
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import Conf_SQL as fcon
@@ -38,26 +39,15 @@ class Start_Page:
         points = []
         
         fcon.check_conf_file()
+        date_lst = []
+        fcon.read_conf_file(date_lst)
         
         self.parent = parent
         
         points = self.take_the_middle(self.parent, points)
-        print(points)
 
         self.frame = ttk.Frame(window)
         self.frame.pack(fill="both", expand=True)
-
-        self.welcm_lbl = ttk.Label(self.frame, text='welcome')
-        self.welcm_lbl.place(x = 300, y = 20)
-
-        self.name_lbl = ttk.Label(self.frame, text='name:')
-        self.name_lbl.place(x = 300, y = 50)
-
-        self.name_entry = ttk.Entry(self.frame)
-        self.name_entry.place(x = 300, y = 80)
-
-        self.sbt = ttk.Button(self.frame, text='login', command=self.clicked)
-        self.sbt.place(x = 300, y = 120)
         
         self.img = Image.open("fmc_logo.png")
         width, height = self.img.size
@@ -65,6 +55,36 @@ class Start_Page:
         self.tkimage = itk.PhotoImage(image = self.img_new_size)
         self.label_img = ttk.Label(self.frame, image=self.tkimage)
         self.label_img.place(x = ((points[2]-int(width/8))/2), y = ((points[1]-int(height/8))/2))
+        
+        self.welcm_lbl = ttk.Label(self.frame, text='DB Connect')
+        self.welcm_lbl.place(x = points[4], y = (((points[1]-int(height/8))/2) + 0))
+
+        self.name_lbl1 = ttk.Label(self.frame, text='Server:')
+        self.name_lbl1.place(x = points[4], y = (((points[1]-int(height/8))/2) + 26))
+
+        self.name_entry1 = WPEntry(self.frame, date_lst[0][1])
+        self.name_entry1.place(x = points[4], y = (((points[1]-int(height/8))/2) + 46))
+        
+        self.name_lbl2 = ttk.Label(self.frame, text='Database:')
+        self.name_lbl2.place(x = points[4], y = (((points[1]-int(height/8))/2) + 76))
+
+        self.name_entry2 = WPEntry(self.frame, date_lst[1][1])
+        self.name_entry2.place(x = points[4], y = (((points[1]-int(height/8))/2) + 96))
+        
+        self.name_lbl3 = ttk.Label(self.frame, text='Username:')
+        self.name_lbl3.place(x = points[4], y = (((points[1]-int(height/8))/2) + 126))
+
+        self.name_entry3 = WPEntry(self.frame, date_lst[2][1])
+        self.name_entry3.place(x = points[4], y = (((points[1]-int(height/8))/2) + 146))
+        
+        self.name_lbl4 = ttk.Label(self.frame, text='Password:')
+        self.name_lbl4.place(x = points[4], y = (((points[1]-int(height/8))/2) + 176))
+
+        self.name_entry4 = WPEntry(self.frame, date_lst[3][1])
+        self.name_entry4.place(x = points[4], y = (((points[1]-int(height/8))/2) + 196))
+
+        self.sbt = ttk.Button(self.frame, text='Login', command=self.clicked)
+        self.sbt.place(x = points[4], y = (((points[1]-int(height/8))/2) + 240))
         
         
     def clicked(self):
@@ -83,8 +103,8 @@ class Start_Page:
         lis.append(int(mid_w)) # 2
         mid_h = d_height / 2
         lis.append(int(mid_h)) # 3
-        mid_w_left_half = mid_w / 2
-        lis.append(int(mid_w_left_half)) # 4
+        mid_w_r_half = (mid_w / 3) + mid_w
+        lis.append(int(mid_w_r_half)) # 4
 
         return lis
 
@@ -111,3 +131,21 @@ class Sign_Page():
     def clicked(self):
         self.frame.destroy()
         self.parent.changepage(0)
+
+class WPEntry(ttk.Entry):
+        
+        def __init__(self, master=None, placeholder="PLACEHOLDER"):
+            
+            super().__init__(master)
+
+            self.placeholder = placeholder
+
+            self.bind("<FocusIn>")
+            self.bind("<FocusOut>")
+
+            self.put_placeholder()
+
+        def put_placeholder(self):
+            
+            self.insert(0, self.placeholder)
+            
